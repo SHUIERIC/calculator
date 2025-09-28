18479983b4a9b80b33ea7cd1d99f081edd63c9e4
SHUIERIC <229310374+SHUIERIC@users.noreply.github.com>
2025-09-28 17:20:54 +1000

    calc: implement operate and display result

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 // Calculation functions 
 
 function add (a,b) {
@@ -19,12 +16,8 @@ function divide (a,b) {
     return a / b
 }
 
-let numOne
-let operator
-let numTwo
 
-
-function operate (numOne, numTwo, operator) {
+/* function operate (numOne, numTwo, operator) {
     switch (operator) {
         case add:
             return add (numOne, numTwo);
@@ -40,6 +33,97 @@ function operate (numOne, numTwo, operator) {
             break;
     }
 }
+*/
 
-console.log (operate(3,2,multiply))
+//Button
+
+let displayValue = ""
+
+const buttons = document.querySelectorAll (".key")
+const display = document.querySelector(".display")
+
+buttons.forEach((button) =>{
+    button.addEventListener ("click", (e)=>{
+    const value = e.target.textContent;
+    displayValue += value;
+    display.textContent = displayValue
+    })
+}) 
+    
+
+// User Input Variable 
+function splitNum (input) {
+    let numOne;
+    let operator;
+    let numTwo;
+
+    if (input.includes ("+")) {
+        [numOne, numTwo] = input.split("+");
+        operator = "+"
+    } else if (input.includes ("-")) {
+        [numOne, numTwo] = input.split("-");
+        operator = "-"
+    } else if (input.includes ("x")) {
+        [numOne, numTwo] = input.split("x");
+        operator = "x"
+    } else if (input.includes ("/")) {
+        [numOne, numTwo] = input.split("/");
+        operator = "/"
+} 
+
+    numOne= parseInt (numOne);
+    numTwo= parseInt (numTwo);
+
+    return result = {numOne, numTwo, operator}
+}
+
+function operate ( {numOne, numTwo, operator} ) {
+
+    switch (operator) {
+        case "+":
+            return add (numOne, numTwo);
+            break;
+        case "-": 
+            return subtract (numOne, numTwo);
+            break;
+        case "x": 
+            return multiply (numOne, numTwo);
+            break;
+        case "/": 
+            return divide (numOne, numTwo);
+            break;
+    }
+}
+
+const equalBtn = document.querySelector (".equal")
+equalBtn.addEventListener("click", ()=> {
+    const finalResult = operate(splitNum(displayValue));
+    display.textContent = finalResult;    
+}
+)
+
+let firstNum = undefined;
+
+const operatorBtn = document.querySelectorAll (".operator")
+operatorBtn.forEach ( (btn) =>
+    btn.addEventListener ("click", (e) => {
+        if (firstNum === undefined) {
+            firstNum = parseInt (displayValue)
+            currentOperator = e.target.textContent
+            display.textContent = `${firstNum}${currentOperator}`;
+            displayValue = ""
+        } else {
+            let secondNum = parseInt(displayValue)
+            const intermediateResult = operate({numOne: firstNum, numTwo: secondNum, operator: currentOperator}) 
+
+            firstNum = intermediateResult;
+            currentOperator = e.target.textContent;
+            display.textContent = `${intermediateResult}${currentOperator}`
+            displayValue = ""
+        }
+        }
+    )
+)
+
+
 
